4101461eb45e37b5433e4d75f6befca20a070e9f
pascalyazid <pascal.thuma@gmail.com>
2022-03-21 15:18:36 +0100

    CSS for collection

--- inventory.php
+++ inventory.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/card.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventar</title>
@@ -27,7 +29,6 @@
                     }
                     echo "You are now logged in as " . htmlspecialchars($_SESSION["username"]);
     ?>
-    <div id="cards">
     <?php
                 $servername = "eu-cdbr-west-02.cleardb.net";
                 $username = "b3ff51d972250f";
@@ -37,35 +38,46 @@
                 $conn = new mysqli($servername, $username, $password, $dbName);
                 $query = "SELECT img_url from card where card_id in (select card_id from card_user where user_id='" . $_SESSION["id"] . "')";
                 //echo $query;
+                $counter = 0;
                 if ($result = mysqli_query($conn, $query)) {
                     // Fetch one and one row
                     
                     while ($row = mysqli_fetch_row($result)) {
-
+                        if($counter == 0) {
+                            echo "<row class='row'>";
+                        }   
                         echo <<<CARD
-                                <div id="card" class="card>
-                                    <div class="card-inner">
-                                        <div class="card-front>
-                                        <img style="width: 350px; height: 600px;" srcset="https://i.imgur.com/P7qYTcI.png">
+                                <div class="col-3">
+                                    <div id="card" class="card">
+                                        <div class="card-inner">
+                                            <div class="card-front>
+                                                <img style="width: 350px; height: 600px;" srcset="https://i.imgur.com/P7qYTcI.png">
                             CARD;
                     
 
                         echo <<<CARD
 
-                                    </div
-                                <div class="card-back">
-                                <img style="width: 350px; height: 600px;" srcset="
+                                            </div
+                                            <div class="card-back">
+                                                <img style="width: 350px; height: 600px;" srcset="
                             CARD;
 
                         echo $row[0];
 
                         echo <<<CARD
 
-                                "></img>
-                                </div>
-                                </div>
+                                                "></img>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             CARD;
+                            $counter++;
+                            if($counter == 4) {
+                                echo "</row>";
+                                $counter = 0;
+                            }
+                            
 
                             
                     }
